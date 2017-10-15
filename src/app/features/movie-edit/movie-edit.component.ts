551e87cf00f62c19d271fd79c31372297afe9025
nikol0020 <6.02Tatyana@gmail.com>
2017-10-15 16:41:33 +0300

    upgraded form movie-edit

--- src/app/features/movie-edit/movie-edit.component.ts
+++ src/app/features/movie-edit/movie-edit.component.ts
@@ -1,8 +1,22 @@
 import {Component} from "@angular/core";
+import {Router, ActivatedRoute} from "@angular/router";
+import {MovieService} from "../../core/services/movie.service";
+
 @Component ({
   moduleId : module.id,
   selector: 'movie-edit',
   templateUrl: 'movie-edit.component.html',
   styleUrls: ['movie-edit.component.scss']
 })
-export class MovieEditComponent{}
+export class MovieEditComponent{
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: MovieService
+  ) {}
+
+  closeMovieItem(): void {
+    this.router.navigate(['/movie-list']);
+  }
+}
